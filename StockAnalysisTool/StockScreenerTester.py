024e4512ce95963d8355c4aaff2410ce8e0d8ff9
SamuelHaupt <sg.haupt@icloud.com>
2021-07-23 08:39:33 -0600

    Added StockScreener unit testing py

---
+++ StockAnalysisTool/StockScreenerTester.py
@@ -0,0 +1,22 @@
+import unittest
+from StockScreener import Stock 
+
+
+class StockScreenerTester(unittest.TestCase):
+
+    def test_stock_1(self):
+        stock = Stock('aapl')
+        
+
+
+
+
+
+
+
+
+# Tests whether file is ran as script and whether the main function ought be called.
+if __name__ == '__main__':
+
+    print()
+    unittest.main()
