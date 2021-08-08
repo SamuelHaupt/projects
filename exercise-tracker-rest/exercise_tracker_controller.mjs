100b9b2077b01a5fafea9baef6d7ee1015cb2270
Samuel Haupt <sg.haupt@icloud.com>
2021-08-08 17:36:11 -0600

    cleaned controller of static and urlencoded.

--- exercise-tracker-rest/exercise_tracker_controller.mjs
+++ exercise-tracker-rest/exercise_tracker_controller.mjs
@@ -2,13 +2,7 @@ import express from 'express';
 import * as exercises from './exercise_tracker_model.mjs';
 
 const PORT = 3000;
-
 const app = express();
-app.use(express.static('public'));
-app.use(express.urlendcoded({
-    extended: true
-}));
-app.use(express.json());
 
 /**
  * Create new exercise.
