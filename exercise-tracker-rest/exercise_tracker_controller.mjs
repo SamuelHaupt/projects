9fe3e4a5deb4429c2ba52228dd421861db9f5083
Samuel Haupt <sg.haupt@icloud.com>
2021-08-08 15:09:53 -0600

    crud: c in controller

--- exercise-tracker-rest/exercise_tracker_controller.mjs
+++ exercise-tracker-rest/exercise_tracker_controller.mjs
@@ -1,20 +1,33 @@
 import express from 'express';
+import * as exercises from './exercise_tracker_model.mjs';
 
 const PORT = 3000;
 
 const app = express();
-
 app.use(express.static('public'));
-
 app.use(express.urlendcoded({
     extended: true
 }));
-
 app.use(express.json());
 
 /**
- * Create an entry for new exercise ...
+ * New exercise endpoint. Create new exercise.
  */
-app.post('', (request, response) => {
-    
+app.post('/exercises', (request, response) => {
+    exercises.createExercise(   request.body.name,
+                                request.body.repetitions,
+                                request.body.weight,
+                                request.body.unitMeasurement,
+                                request.body.date)
+        .then(exercise => {
+            // Sets status code to 'created': 201.
+            response.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            // Sets status code to 'bad request': 400.
+            // Future expansion: Include code to test error and set 
+            // status code accordingly.
+            response.status(400).json({Error: 'Request failed.'});
+        });
 });
