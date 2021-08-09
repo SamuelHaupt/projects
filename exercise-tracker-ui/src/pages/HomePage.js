c5bded9e4035ab7d01201bc93e3538baa9c7b210
Samuel Haupt <sg.haupt@icloud.com>
2021-08-08 20:04:47 -0600

    Organized code & added fetch function to db

--- exercise-tracker-ui/src/pages/HomePage.js
+++ exercise-tracker-ui/src/pages/HomePage.js
@@ -1,9 +1,19 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import ExerciseTable from '../components/ExerciseTable';
 
 function HomePage() {
     const [exercises, setExercises] = useState([]);
+
+    const loadExercises = async () => {
+        const response = await fetch('/exercises');
+        const data = await response.json();
+        setExercises(data);
+    }
+
+    useEffect(() => {
+        loadExercises();
+    }, []);
 
     return (
         <>
