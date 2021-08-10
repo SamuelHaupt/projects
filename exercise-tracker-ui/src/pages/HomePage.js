1a520ce5de6b745776c33221b9615b88701141d6
Samuel Haupt <sg.haupt@icloud.com>
2021-08-09 20:02:29 -0600

    Added onDelete and onEdit

--- exercise-tracker-ui/src/pages/HomePage.js
+++ exercise-tracker-ui/src/pages/HomePage.js
@@ -2,8 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import ExerciseTable from '../components/ExerciseTable';
 
-function HomePage() {
+function HomePage({ setExerciseToEdit }) {
     const [exercises, setExercises] = useState([]);
+    const history = useHistory();
+
+    const onDelete = async id => {
+        const response = await fetch(`/exercises/${id}`, { method: 'DELETE' });
+        if (response.status === 204) {
+            const getResponse = await fetch('/exercises');
+            const exercises = await getResponse.json();
+            setExercises(exercises);
+        } else {
+        console.error(`Failed to delete the exercise with id = ${id}, status code = ${response.status}`)
+        }
+    }	
+
+    const onEdit = exercise => {
+        setExerciseToEdit(exercise);
+        history.push('/edit-exercise');
+    }
 
     const loadExercises = async () => {
         const response = await fetch('/exercises');
@@ -18,7 +35,7 @@ function HomePage() {
     return (
         <>
             <h2>List of Exercises</h2>
-            <ExerciseTable exercises={exercises}></ExerciseTable>
+            <ExerciseTable exercises={exercises} onDelete={onDelete} onEdit={onEdit}></ExerciseTable>
             <Link to='/create-exercise'>Add exercise</Link>
         </>
     )
