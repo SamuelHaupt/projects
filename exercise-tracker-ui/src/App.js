32830727713aa805ad6b7b221cc530d673bebf56
Samuel Haupt <sg.haupt@icloud.com>
2021-08-09 20:05:27 -0600

    useState at parent passed to Home & EditExercisePg

--- exercise-tracker-ui/src/App.js
+++ exercise-tracker-ui/src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateExercisePage from './pages/CreateExercisePage';
 import EditExercisePage from './pages/EditExercisePage';
 
 function App() {
+  const [exerciseToEdit, setExerciseToEdit] = useState();
+
   return (
     <div className="App">
       <Router>
         <header className="App-header">
           <Route path='/' exact>
-            <HomePage />
+            <HomePage setExerciseToEdit={setExerciseToEdit}/>
           </Route>
           <Route path='/create-exercise'>
             <CreateExercisePage />
           </Route>
           <Route path='/edit-exercise'>
-            <EditExercisePage />
+            <EditExercisePage exerciseToEdit={exerciseToEdit}/>
           </Route>
         </header>
       </Router>
