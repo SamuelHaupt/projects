d4c1f5728f9f5b1298d95e25b53d3e7adaea75c0
Samuel Haupt <sg.haupt@icloud.com>
2021-11-11 09:36:04 -0700

    Added sample data input

--- database/ddl.sql
+++ database/ddl.sql
@@ -26,7 +26,7 @@ CREATE TABLE `Employees` (
 
 DROP TABLE IF EXISTS paystubs;
 CREATE TABLE `PayStubs` (
-    `payStubsID` int(12) NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    `payStubID` int(12) NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     `employeeID` int(7) NOT NULL,
     `payDate` date NOT NULL,
     `payRate` decimal(7,2) NOT NULL,
@@ -34,10 +34,14 @@ CREATE TABLE `PayStubs` (
 ) ENGINE=InnoDB;
 
 ## Change payDate to date instead of char.
+## Delete UNIQUE from officeSiteID in outline.
+## officeSiteID cannot be NULL because it's a primary key. Should remote = 0?
+## Also, we made officeSiteID null-able because of a request from the project assignment.
+## Delete plural of payStubID in outline.
 
 DROP TABLE IF EXISTS employees_officesites;
 CREATE TABLE `Employees_OfficeSites` (
-    `officeSiteID` int(4) UNIQUE,
+    `officeSiteID` int(4),
     `employeeID` int(7) NOT NULL UNIQUE,
     PRIMARY KEY (`officeSiteID`, `employeeID`)
 ) ENGINE=InnoDB;
@@ -54,8 +58,75 @@ ADD FOREIGN KEY `employeeID_FK` (`employeeID`) REFERENCES `Employees` (`employee
 
 SET FOREIGN_KEY_CHECKS = 1;
 
-DESCRIBE Departments;
-DESCRIBE OfficeSites;
-DESCRIBE PayStubs;
-DESCRIBE Employees_OfficeSites;
-DESCRIBE Employees;
+## Data Dump
+
+LOCK TABLES `Departments` WRITE;
+/*!40000 ALTER TABLE `Departments` DISABLE KEYS */;
+
+INSERT INTO `Departments` (`name`) VALUES
+    ('Mortgage Lending'),
+    ('Investment Banking'),
+    ('Personal Banking');
+
+/*!40000 ALTER TABLE `Departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `OfficeSites` WRITE;
+/*!40000 ALTER TABLE `OfficeSites` DISABLE KEYS */;
+
+INSERT INTO `OfficeSites` (`officeSiteID`, `address`) VALUES
+    (202, '154 Sunset Boulevard, Bayview, California 90005'),
+    (205, '2005 West Hills Drive, Los Angeles, California 90006'),
+    (210, '5001 Santa Monica Boulevard, Santa Monica, California 90011'),
+    (NULL, '5050 Santa Clara Boulevard, Santa Monica, California 90011'),
+    (9999, 'REMOTE');
+
+/*!40000 ALTER TABLE `OfficeSites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `Employees` WRITE;
+/*!40000 ALTER TABLE `Employees` DISABLE KEYS */;
+
+INSERT INTO `Employees` (`departmentID`, `firstName`, `lastName`) VALUES
+    (1, 'Nate', 'Macauley'),
+    (1, 'Bronwyn', 'Rojas'),
+    (2, 'Addy', 'Prentiss'),
+    (3, 'Cooper', 'Clay');
+
+/*!40000 ALTER TABLE `Employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `Employees_OfficeSites` WRITE;
+/*!40000 ALTER TABLE `Employees_OfficeSites` DISABLE KEYS */;
+
+INSERT INTO `Employees_OfficeSites` (`officeSiteID`, `employeeID`) VALUES
+    (202, 1),
+    (202, 2),
+    (210, 3),
+    (9999, 4);
+
+/*!40000 ALTER TABLE `Employees_OfficeSites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `PayStubs` WRITE;
+/*!40000 ALTER TABLE `PayStubs` DISABLE KEYS */;
+
+INSERT INTO `PayStubs` (`payStubID`, `employeeID`, `payDate`, `payRate`, `hoursWorked`) VALUES
+    (4554, 2, '2021-06-17', 35.23, 40),
+    (NULL, 2, '2021-06-30', 35.23, 39),
+    (6512, 3, '2021-07-17', 30.10, 40),
+    (NULL, 3, '2021-07-17', 30.10, 40);
+
+/*!40000 ALTER TABLE `PayStubs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+SELECT * FROM Departments;
+SELECT * FROM OfficeSites;
+SELECT * FROM Employees;
+SELECT * FROM Employees_OfficeSites;
+SELECT * FROM PayStubs;
