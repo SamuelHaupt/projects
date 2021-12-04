373485af6af30a897978eb6aca1e6fd0f82537ed
eemcmullan <eemcmullan92@gmail.com>
2021-12-04 13:24:43 -0500

    update dmq.sql

--- database/dmq.sql
+++ database/dmq.sql
@@ -4,15 +4,18 @@ Departments Table
 -- Display all departments
 SELECT * FROM Departments;
 
+
 -- Add department
 INSERT INTO Departments name
 VALUES :name;
+
 
 /***************
 PayStubs Table
 ****************/
 -- Display all paystubs
 SELECT * FROM PayStubs;
+
 
 -- Add paystub
 INSERT INTO PayStubs (employeeID, payDate, payRate, hoursWorked)
@@ -25,9 +28,11 @@ OfficeSites Table
 -- Display all office sites
 SELECT * FROM OfficeSites;
 
+
 -- Add office site
 INSERT INTO OfficeSites address
 VALUES :address;
+
 
 /*****************************************
 Employees & Employees_OfficeSites Tables 
@@ -36,10 +41,19 @@ Employees & Employees_OfficeSites Tables
 SELECT * FROM Employees;
 SELECT * FROM Employees_OfficeSites;
 
+
 -- Search for employee 
-SELECT * FROM Employees WHERE :chosenFilter = :employeeFilter;
+SELECT employeeID, officeSiteID FROM Employees_OfficeSites 
+WHERE officeSiteID = :officeSiteID;
+
+SELECT * FROM Employees_OfficeSites WHERE employeeID IN :filter_val;
+
+SELECT * FROM Employees WHERE selectedFilter = :employeeFilter;
+
 SELECT employeeID FROM Employees WHERE :chosenFilter = :employeeFilter;
-SELECT * FROM Employees_OfficeSites WHERE employeeID IN :employeeIDSubquery;
+
+SELECT * FROM Employees_OfficeSites WHERE employeeID IN :employeeID;
+
 
 -- Add employee and employee_oddiceSite
 SELECT * FROM Employees WHERE employeeID = :employeeID;
@@ -47,14 +61,14 @@ SELECT * FROM Employees WHERE employeeID = :employeeID;
 INSERT INTO Employees (firstName, lastName, departmentID)
 VALUES (:first_name, :last_name, :department_id);
 
-SELECT * FROM Employees_OfficeSites WHERE employeeID IN :employeeID;
-
 INSERT INTO Employees_OfficeSites (officeSiteID, EmployeeID)
 VALUES (:officeSite_id, :employeeID);
+
 
 -- Delete employee from Employees table and Employees_OfficeSites table
 DELETE FROM Employees_OfficeSites WHERE employeeID = :employee_id;
 DELETE FROM Employees WHERE employeeID = :employee_id;
+
 
 -- Update employee first name and last name by employee id
 -- Update Employees_OfficeSites by employeeID
