a7faae688ad804976d0703e6639b0950a005b2e7
Samuel Haupt <sg.haupt@icloud.com>
2023-10-19 23:23:54 -0600

    ATBP-1 ATBP-2 changed class name for yF and added main.py

--- data_processor.py
+++ data_processor.py
@@ -1,21 +1,20 @@
-# flake8: noqa
-# import gymnasium as gym
+"""
+Downloader module downloads data from Yahoo Finance using yFinance module.
+It also provides a function to clean the data.
+"""
+
 import pandas as pd
 import yfinance as yf
-import os
-# import gym_trading_env
 
-from datetime import date
-from datetime import timedelta
 
-class YFDownloader():
+class DataProcessor():
 
     def __init__(self) -> None:
         self.symbol = None
         self.start_date = None
         self.end_date = None
-    
-    def download_data_df(
+
+    def download_data_df_from_yf(
         self,
         symbol: str,
         start_date: str,
@@ -30,6 +29,15 @@ class YFDownloader():
 
         Returns:
             pd.DataFrame: Dataframe for requested symbol
+
+        Example pd.DataFrame.head():
+                     Open   High    Low  Close    Volume
+        Date
+        2021-02-01  45.96  48.16  45.07  47.71  52829800
+        2021-02-02  49.07  50.58  49.01  50.02  46373200
+        2021-02-03  51.06  51.20  49.44  49.44  47586000
+        2021-02-04  50.13  51.24  49.49  51.19  36404400
+        2021-02-05  51.68  52.21  50.88  51.73  43307600
         """
 
         start_date = pd.Timestamp(start_date)
@@ -41,7 +49,7 @@ class YFDownloader():
 
         # download and save the data to pandas DataFrame
         data_df = yf.download(
-            tickers = self.symbol,
+            tickers=self.symbol,
             start=self.start_date,
             end=self.end_date,
             interval=time_interval,
@@ -54,7 +62,7 @@ class YFDownloader():
 
     def clean_data(self, data_df: pd.DataFrame) -> pd.DataFrame:
         """Cleans data by using SPY's Close non-Nan, rather than grabbing
-        New York Stock Exchanges trading days. This is a workaround to 
+        New York Stock Exchanges trading days. This is a workaround to
         simply the amount of libraries required to clean data. ffill and bfill
         are applied to normalize data.
 
@@ -66,8 +74,9 @@ class YFDownloader():
         """
         spy = self.download_data_df('SPY', self.start_date, self.end_date)
         spy.dropna(subset='Close', inplace=True)
-        spy.drop(columns=['Open', 'High', 'Low', 'Close', 'Volume'], inplace=True)
-        
+        spy.drop(columns=['Open', 'High', 'Low', 'Close', 'Volume'],
+                 inplace=True)
+
         # use trading days from SPY to clean data.
         new_df = spy.join(data_df, how='left')
 
@@ -79,9 +88,11 @@ class YFDownloader():
 
 
 if __name__ == "__main__":
-    yf_downloader = YFDownloader()
+    data_processor = DataProcessor()
     symbol = 'SPY'
     start_date = '2021-01-30'
     stop_date = '2022-01-30'
-    data_df = yf_downloader.download_data_df(symbol, start_date, stop_date)
-    clean_data_df = yf_downloader.clean_data(data_df)
+    data_df = data_processor.download_data_df_from_yf(symbol,
+                                                      start_date,
+                                                      stop_date)
+    clean_data_df = data_processor.clean_data(data_df)
