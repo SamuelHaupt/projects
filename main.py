c22871bf3c9a3c965e3e0b0dbf66e3d493c7d8db
James Mejia <63440661+JMejia0482@users.noreply.github.com>
2023-10-23 15:35:08 -0600

    Environment
    Added Trading Environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from data_processor import DataProcessor
-
+import gymnasium as gym
+import gym_trading_env
+import pandas as pd
 
 def main():
     data_processor = DataProcessor()
@@ -12,6 +14,38 @@ def main():
     tqqq_preprocessed = data_processor.preprocess_data(tqqq)
 
     print(tqqq_preprocessed.head())
+
+    # CREATE ENVIRONMENT
+    df = pd.read_csv("data/binance-BTCUSDT-1h.csv")
+
+    # Format table date proper format and name
+    df["Date"] = pd.to_datetime(df["date_open"])
+    df.dropna(inplace=True)  # Clean again !
+
+    # Return to the first row
+    df.head()
+
+    # Create Trading Environment
+    training_env = gym.make("TradingEnv",
+                            name="BTCUSD",
+                            df=df,
+                            windows=5,
+                            positions=[0, 1],  # 0(=SELL ALL), +1 (=BUY ALL)
+                            initial_position=0,
+                            portfolio_initial_value=1000,
+                            )
+
+    truncated = False
+    observation, info = training_env.reset()
+    while True and not truncated:
+        position_index = training_env.action_space.sample()
+        observation, reward, done, truncated, info = training_env.step(position_index)
+
+    print("Info:", info)
+    print("Observation:", observation)
+    print("Portfolio Value:", info.get("portfolio_valuation"))
+    training_env.close()
+    print("Testing Environment Complete!")
 
 
 if __name__ == '__main__':
