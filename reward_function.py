2f6332962f716ff0dbf579767d4eb607155f7e3f
davsface <davejhuston@gmail.com>
2023-10-23 16:47:53 -0700

    ATBP-5 added reward function that uses sortino ratio

--- reward_function.py
+++ reward_function.py
@@ -3,14 +3,14 @@ import numpy as np
 from gym_trading_env.utils.history import History
 
 
-def reward_function(history: History, win_size: int = 144) -> float:
+def reward_function_drawdown(history: History, win_size: int = 144) -> float:
     """
     Reward Function
 
     Args:
         History 
         Window Size
-        
+
     Returns:
         risk weighted positive profit - risk weighted drawdown
     """
@@ -40,3 +40,38 @@ def reward_function(history: History, win_size: int = 144) -> float:
     reward = (profit_reward_weight * positive_profit) - ((1 - profit_reward_weight) * mean_drawdown)
 
     return reward
+
+
+def reward_function_sortino(history: History, win_size: int = 144) -> float:
+    """
+    Reward Function
+
+    Args:
+        History 
+        Window Size
+
+    Returns:
+        Sortino ratio
+    """
+    # adjust based on some baseline
+    risk_free_rate = 0.0
+
+    # pull data from history
+    data_close = np.array(history['data_close'][-win_size:], dtype=np.float64)
+    pos = np.array(history['position'][-win_size:], dtype=np.float64)
+
+    # calculate log returns for the rolling window 
+    log_data = np.log(data_close)
+    log_return = np.where(pos[1:] == 1, np.diff(log_data), 0)
+
+    # Calculate average profit in the window
+    average_profit = np.mean(np.where(log_return > 0, log_return, 0))
+
+    # Calculate average loss in the window
+    average_loss = np.mean(np.where(log_return < risk_free_rate, log_return^2, 0))
+
+    reward = average_profit/(average_loss)^0.5
+
+    return reward
+
+
