6e559ba9f8d40a252c9e5ee308c3828a445bcac4
James Mejia <63440661+JMejia0482@users.noreply.github.com>
2023-10-28 16:39:46 -0600

    Update main.py
    Tied up downloading stock/asset from yfinance. Once downloaded, the downloaded dataframe needs to be formatted to gym-trader-env format. It them creates two different dataframes, one for training, another for testing. The training environment is initialized with the training dataframe. A PPO model is created and saved. It then uses the model and runs in testing environment and outputs the result.
    
    *RecurrentPPO doesn't work on Mac, so I use PPO. You can switch back and forth depending on your platform.

--- main.py
+++ main.py
@@ -1,51 +1,76 @@
+import numpy as np
+from stable_baselines3 import PPO
+import reward_function
+from agent_module import PPOAgentModule
 from data_processor import DataProcessor
 import gymnasium as gym
 import gym_trading_env
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+
 import pandas as pd
+
 
 def main():
     data_processor = DataProcessor()
     symbol = 'TQQQ'
-    start_date = '2021-01-30'
-    stop_date = '2022-01-30'
+    start_date = '2015-01-01'
+    stop_date = '2023-10-01'
     tqqq = data_processor.download_data_df_from_yf(symbol,
                                                    start_date,
                                                    stop_date)
-    tqqq_preprocessed = data_processor.preprocess_data(tqqq)
-
-    print(tqqq_preprocessed.head())
-
-    # CREATE ENVIRONMENT
-    df = pd.read_csv("data/binance-BTCUSDT-1h.csv")
+    tqqq_preprocessed = data_processor.preprocess_data(tqqq, 50)
 
     # Format table date proper format and name
-    df["Date"] = pd.to_datetime(df["date_open"])
-    df.dropna(inplace=True)  # Clean again !
+    tqqq_preprocessed.dropna(inplace=True)  # Clean again !
 
-    # Return to the first row
+    # Format to gym-trader-env format
+    df = pd.DataFrame([], [])
+    for column in tqqq_preprocessed.columns:
+        df[str(column).lower()] = pd.DataFrame(tqqq_preprocessed[column].values, columns=[column])
+    df["date"] = pd.DataFrame(tqqq_preprocessed["Close"].index, columns=["Date"])
     df.head()
 
-    # Create Trading Environment
+    # Setup training data
+    training_df = df[df["date"] <= "2022-12-31"]
+    training_df.dropna(inplace=True)
+    training_df.head()
+
+    # Setup testing data
+    testing_df = df[df["date"] > "2022-12-31"]
+    testing_df.dropna(inplace=True)
+    testing_df.head()
+
+    # Load training environment
     training_env = gym.make("TradingEnv",
-                            name="BTCUSD",
-                            df=df,
-                            windows=5,
-                            positions=[0, 1],  # 0(=SELL ALL), +1 (=BUY ALL)
-                            initial_position=0,
+                            df=training_df,
+                            positions=[0, 1],
+                            initial_position=1,
                             portfolio_initial_value=1000,
-                            )
+                            reward_function=reward_function.reward_function_drawdown)
 
-    truncated = False
-    observation, info = training_env.reset()
-    while True and not truncated:
-        position_index = training_env.action_space.sample()
-        observation, reward, done, truncated, info = training_env.step(position_index)
+    # agent = PPOAgentModule(training_env)
+    # agent.train(10000)
 
-    print("Info:", info)
-    print("Observation:", observation)
-    print("Portfolio Value:", info.get("portfolio_valuation"))
-    training_env.close()
-    print("Testing Environment Complete!")
+    # Load Model
+    agent = PPO.load("models/20231028161306_ppo_trading_agent.zip")
+    print(agent)
+
+    # Load testing environment
+    testing_env = gym.make("TradingEnv",
+                           df=testing_df,
+                           positions=[0, 1],
+                           initial_position=1,
+                           portfolio_initial_value=1000,
+                           reward_function=reward_function.reward_function_drawdown)
+
+    observation, info = testing_env.reset()
+    print("# Testing model on testing data...")
+    for _ in range(len(testing_df)):
+        position_index, _states = agent.predict(observation)
+        observation, reward, done, truncated, info = testing_env.step(position_index)
+        if done or truncated:
+            break
 
 
 if __name__ == '__main__':
