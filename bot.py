64e7e7b085b26d39b4ce21652b5b3cd5177c1e5c
bnoora <bnooranii@gmail.com>
2023-11-03 13:59:50 -0700

    Added trading functionality to boy.py, needs api keys to function Added alpaca to requirements.txt

--- bot.py
+++ bot.py
@@ -1,16 +1,54 @@
 from sb3_contrib import RecurrentPPO
 from data_processor import DataProcessor
 import gymnasium as gym
-import gym_trading_env
 import pandas as pd
-import numpy as np
 from reward_function import drawdown
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
+from alpaca.trading.enums import AssetClass
+from alpaca.trading.enums import OrderSide, TimeInForce
+
+trading_client = TradingClient('api-key', 'secret-key', paper=True)
+account = trading_client.get_account()
+
+
+def main():
+    '''
+    main function of the bot.
+    Args:
+        None
+    Returns:
+        None
+    '''
+    # get assets
+    assets = get_assets()
+    print(f"Assets: {assets}")
+
+    # get action
+    trade_decision = get_action()
+
+    # trade
+    trade(trade_decision)
+
+
+def get_assets():
+    '''
+    This function gets all the assets from the Alpaca API.
+    Args:
+        None
+    Returns:
+        assets (list): list of assets
+    '''
+    search_params = GetAssetsRequest(asset_class=AssetClass.CRYPTO)
+    assets = trading_client.get_all_assets(search_params)
+    return assets
+
 
 def get_action() -> str:
     '''
     This function gets the action from the model and returns a trade decision.
 
-    Args: 
+    Args:
         None
     Returns:
         String: trade decision
@@ -22,20 +60,18 @@ def get_action() -> str:
     symbol = 'TQQQ'
     start_date = '2015-01-01'
     stop_date = '2023-10-01'
-    tqqq = data_processor.download_data_df_from_yf(symbol,
-                                                    start_date,
-                                                    stop_date)
+    tqqq = data_processor.download_data_df_from_yf(
+        symbol, start_date, stop_date)
     tqqq_preprocessed = data_processor.preprocess_data(tqqq, 50)
 
-
-    tqqq_preprocessed.dropna(inplace=True) 
+    tqqq_preprocessed.dropna(inplace=True)
 
     df = pd.DataFrame([], [])
     for column in tqqq_preprocessed.columns:
-        df[str(column).lower()] = pd.DataFrame(tqqq_preprocessed[column].values, columns=[column])
+        df[str(column).lower()] = pd.DataFrame(
+            tqqq_preprocessed[column].values, columns=[column])
     df["date"] = pd.DataFrame(tqqq_preprocessed["Close"].index, columns=["Date"])
     df.head()
-
 
     training_df = df[df["date"] <= "2022-12-31"]
     training_df.dropna(inplace=True)
@@ -43,7 +79,7 @@ def get_action() -> str:
     feature_names = training_df.columns.tolist()
     print(feature_names)
     selected_features = [
-        'volume', 'daily returns', 
+        'volume', 'daily returns',
         'feature_hma', 'feature_velocity', 'feature_3d_shifted_velocity',
         'feature_acceleration', 'feature_3d_shifted_acceleration'
     ]
@@ -57,7 +93,7 @@ def get_action() -> str:
                         reward_function=drawdown)
 
     # get action from the model
-    action, _states = model.predict(observation , deterministic=True)
+    action, _states = model.predict(observation, deterministic=True)
 
     print(f"Action: {action}")
     if action == 1:
@@ -68,3 +104,31 @@ def get_action() -> str:
         trade_decision = 'hold'
     print(f"Trade decision: {trade_decision}")
     return trade_decision
+
+
+def trade(trade_decision: str) -> None:
+    '''
+    Function to perform the crypto trade
+    Args:
+        trade_decision (str): buy, sell, or hold
+    Returns:
+        None
+    '''
+    if trade_decision == 'buy':
+        trading_client.submit_order(
+            symbol='TQQQ',
+            qty=0.5,
+            side=OrderSide.BUY,
+            type='market',
+            time_in_force=TimeInForce.DAY
+        )
+    elif trade_decision == 'sell':
+        trading_client.submit_order(
+            symbol='TQQQ',
+            qty=0.5,
+            side=OrderSide.SELL,
+            type='market',
+            time_in_force=TimeInForce.DAY
+        )
+    else:
+        print('Holding position')
