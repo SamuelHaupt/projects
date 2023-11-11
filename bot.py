7da3e2af9fcfebffcf096fb787ae4bc81d5fa62a
bnoora <bnooranii@gmail.com>
2023-11-10 20:49:41 -0800

    Fixed flake8 and some df issues

--- bot.py
+++ bot.py
@@ -3,15 +3,15 @@ from data_processor import DataProcessor
 from env import AssetTradingEnv
 import pandas as pd
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import GetAssetsRequest
-from alpaca.trading.enums import AssetClass
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
 from datetime import date
 
-
-trading_client = TradingClient('PK81K3G1O76EG5ITK9AQ', 'E9k93RSv1x8ojGmgqd43KmPKAlm44DtEVrCDikel', paper=True)
+key = 'PK81K3G1O76EG5ITK9AQ'
+secret_key = 'E9k93RSv1x8ojGmgqd43KmPKAlm44DtEVrCDikel'
+trading_client = TradingClient(key, secret_key, paper=True)
 account = trading_client.get_account()
+account_balance = float(account.cash)
 
 
 def main():
@@ -26,8 +26,7 @@ def main():
     tqqq_asset = get_specified_asset('TQQQ', all_assets)
     print(f"TQQQ Balance: {tqqq_asset.qty}")
 
-    usd_balance = get_usd_balance()
-    print(f"USD Balance: {usd_balance }")
+    print(f"Account balance: {account_balance}")
 
     # get action
     trade_decision = get_action()
@@ -35,19 +34,6 @@ def main():
 
     # trade
     trade(trade_decision)
-
-
-def get_assets():
-    '''
-    This function gets all the assets from the Alpaca API.
-    Args:
-        None
-    Returns:
-        assets (list): list of assets
-    '''
-    search_params = GetAssetsRequest(asset_class=AssetClass.CRYPTO)
-    assets = trading_client.get_all_assets(search_params)
-    return assets
 
 
 def get_action() -> str:
@@ -59,37 +45,22 @@ def get_action() -> str:
     Returns:
         String: trade decision
     '''
-    account_balance = get_usd_balance()
-    account_balance = float(account_balance)
     model_path = "models/20231110194307_ppo_trading_agent"
     model = RecurrentPPO.load(model_path)
-
     data_processor = DataProcessor()
     symbol = 'TQQQ'
-    # date format YYYY-MM-DD
-    start_date = '2019-01-01'
+
+    start_date = '2010-01-01'
     stop_date = date.today() - pd.Timedelta(days=1)
-    tqqq = data_processor.download_data_df_from_yf(symbol,
-                                                   start_date,
-                                                   stop_date)
-    tqqq_preprocessed = data_processor.preprocess_data(tqqq)
+    # stop_date = '2020-01-01'
+    tqqq = data_processor.download_data_df_from_yf(
+        symbol, start_date, stop_date)
+    trading_df = data_processor.preprocess_data(tqqq)
+    trading_df.dropna(inplace=True)
+    trading_env = AssetTradingEnv(
+        data_df=trading_df,
+        initial_balance=account_balance)
 
-    # Format table date proper format and name
-    tqqq_preprocessed.dropna(inplace=True)  # Clean again !
-
-    # Format to gym-trader-env format
-    df = pd.DataFrame([], [])
-    for column in tqqq_preprocessed.columns:
-        df[str(column).lower()] = pd.DataFrame(tqqq_preprocessed[column].values, columns=[column])
-    df["date"] = pd.DataFrame(tqqq_preprocessed["close"].index, columns=["Date"])
-    df.head()
-
-    # Setup training data
-    trading_df = df[df["date"] <= "2021-12-31"]
-    trading_df = trading_df.dropna()
-    trading_df.head()
-
-    trading_env = AssetTradingEnv(data_df=trading_df, initial_balance=account_balance)
     observation = trading_env._get_obs()
     # print(observation)
 
@@ -132,7 +103,7 @@ def trade(trade_decision: str) -> None:
             side=OrderSide.SELL,
             type='market',
             time_in_force=TimeInForce.DAY)
-        
+
         trading_client.submit_order(market_order_data)
 
     else:
@@ -148,7 +119,7 @@ def get_usd_balance():
         usd_balance (float): The amount of USD available in the account
     '''
     usd_balance = account.cash
-    return usd_balance
+    return float(usd_balance)
 
 
 def get_assets():
@@ -157,9 +128,11 @@ def get_assets():
     Args:
         None
     Returns:
-        bitcoin_balance (dict): A dictionary with details of Bitcoin-related assets
+        bitcoin_balance (dict): A dictionary with details of Bitcoin-related
+        assets
     '''
     return trading_client.get_all_positions()
+
 
 def get_specified_asset(asset_symbol, assets):
     '''
@@ -174,7 +147,6 @@ def get_specified_asset(asset_symbol, assets):
             return asset
     print(f"Asset {asset_symbol} not found")
     return None
-
 
 
 if __name__ == '__main__':
