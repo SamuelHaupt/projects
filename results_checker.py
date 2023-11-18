44394ac28d7ebd3e6c3074f7a2d8d9be1dc5b44a
davsface <davejhuston@gmail.com>
2023-11-18 13:44:27 -0800

    added reward to result output csv

--- results_checker.py
+++ results_checker.py
@@ -3,6 +3,7 @@ import csv
 def calculate_beat(csv_file_path):
     market_returns = []
     agent_returns = []
+    row_count=0
 
     # Read the CSV file
     with open(csv_file_path, 'r') as file:
@@ -10,6 +11,7 @@ def calculate_beat(csv_file_path):
         for row in reader:
             market_returns.append(float(row['Market Return']))
             agent_returns.append(float(row['Agent Return']))
+            row_count+=1
 
     # Calculate the number of times agent return beats market return
     beats_count = sum(agent > market for agent, market in zip(agent_returns, market_returns))
@@ -17,11 +19,11 @@ def calculate_beat(csv_file_path):
     # Calculate the average beat
     average_beat = sum(agent - market for agent, market in zip(agent_returns, market_returns)) / len(agent_returns)
 
-    return beats_count, average_beat
+    return beats_count, average_beat, row_count
 
 if __name__ == '__main__':
     # paste path to results csv here
-    beats_count, average_beat = calculate_beat('./results/results_20231116120058')
+    beats_count, average_beat, row_count = calculate_beat('./results/risk_only_reward_training')
     
-    print(f"Number of times agent return beats market return: {beats_count}")
+    print(f"Number of times agent return beats market return: {beats_count}/{row_count}")
     print(f"Average beat of agent return over market return: {average_beat}")
